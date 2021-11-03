10282a5b7a9a2fe57a4d0b2d55fb7f03c985953f
Huynh Tien Tai <taiht221@gmail.com>
2021-11-03 16:58:37 +0700

    fix11

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  AOS.init()
   function menuTop() {
     if ($(this).scrollTop() >= 300) {
       $('.header').addClass('active')
@@ -13,3 +14,25 @@ $(document).ready(function () {
     $('.menu-right').toggleClass('active')
   })
 })
+
+// Click on a color
+const base = 'images/'
+var el = document.getElementsByClassName('color')
+for (var i = 0; i < el.length; i++) {
+  el[i].onclick = changeColor
+}
+
+function changeColor(e) {
+  // get the hex color
+  let hex = e.target.getAttribute('data-color')
+  // updated url
+  let updateColor = base + hex + '.jpg'
+  // update src of image
+  document.getElementById('productImage').src = updateColor
+  document.getElementById('product-colorName').textContent = `Màu ${hex} - 1K3`
+}
+
+$(document).on('click', '.pick .color', function () {
+  $(this).addClass('color-active').siblings().removeClass('color-active')
+})
+AOS.init()
