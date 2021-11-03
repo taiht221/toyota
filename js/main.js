4d828dba231cafe11f1b30fc11048ec7adf5fca0
Huynh Tien Tai <taiht221@gmail.com>
2021-11-03 11:14:11 +0700

    fix2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-
+$(document).ready(function () {
+  function menuTop() {
+    if ($(this).scrollTop() >= 300) {
+      $('.header').addClass('active')
+    } else {
+      $('.header').removeClass('active')
+    }
+  }
+  menuTop()
+  $(window).scroll(menuTop)
+  $('.hamburger').on('click', function () {
+    $('.hamburger').toggleClass('active')
+    $('.menu-right').toggleClass('active')
+  })
+})
